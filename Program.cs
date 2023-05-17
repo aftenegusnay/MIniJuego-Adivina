9e7293a64981b92a2189116dd2c64de2f6ac83e6
aftenegusnay <102046581+aftenegusnay@users.noreply.github.com>
2023-05-16 23:37:01 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+Console.WriteLine("Adivina el numero");
+Random rnd = new Random();
+int num = rnd.Next(1, 11);
+int entrada = 0;
+Console.WriteLine("Ingresa el numero entero que creas correcto");
+do
+{
+    entrada = int.Parse(Console.ReadLine());
+    if (entrada == num)
+        Console.WriteLine(num + " es el numero correcto");
+    else
+    {
+        if (entrada > num)
+            Console.WriteLine(entrada + " es mayor al numero deseado, intenta de nuevo");
+        else if (entrada < num)
+            Console.WriteLine(entrada + " es menor al numero deseado, intenta de nuevo");
+        else
+            Console.WriteLine("Ingrese un numero valido");
+    }
+} while (entrada != num);
+Console.WriteLine("El numero generado es " + num);
+Console.ReadKey();
